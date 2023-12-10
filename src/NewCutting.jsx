cd38520fd57f7a46a59179db67d12e5e86f86b0f
“Jessica <jmychalm13@gmail.com>
2023-12-10 12:27:04 -0600

    added working dropdowns for form

--- src/NewCutting.jsx
+++ src/NewCutting.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import { ToastNotification } from "./ToastNotification";
 
 export function NewCutting() {
+  const [plants, setPlants] = useState([]);
+  const [zones, setZones] = useState([]);
   const [showToast, setShowToast] = useState(false);
 
   const handleSubmit = (event) => {
@@ -23,15 +25,42 @@ export function NewCutting() {
       });
   };
 
+  const getPlantValues = () => {
+    axios.get("http://localhost:3000/dropdowns/plant.json").then((response) => {
+      setPlants(response.data);
+    });
+  };
+
+  const getZoneValues = () => {
+    axios.get("http://localhost:3000/dropdowns/zone.json").then((response) => {
+      setZones(response.data);
+    });
+  };
+
+  useEffect(getPlantValues, []);
+  useEffect(getZoneValues, []);
+
   return (
     <div>
       <h1>New Cutting</h1>
       {showToast && <ToastNotification message="Cutting Created!" />}
       <form onSubmit={handleSubmit}>
-        <label htmlFor="user_plant_id">Plant ID</label>
-        <input type="integer" name="user_plant_id" />
-        <label htmlFor="zone_id">Zone ID</label>
-        <input type="number" name="zone_id" />
+        <label htmlFor="user_plant_id">Plant:</label>
+        <select name="user_plant_id" id="user_plant_id">
+          {plants.map((plant) => (
+            <option key={plant.id} value={plant.id}>
+              {plant.name}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="zone_id">Zone:</label>
+        <select name="zone_id" id="zone_id">
+          {zones.map((zone) => (
+            <option key={zone.id} value={zone.id}>
+              {zone.location_name}
+            </option>
+          ))}
+        </select>
         <label htmlFor="date_cut">Date Cut</label>
         <input type="date" name="date_cut" />
         <label htmlFor="roots">Roots</label>

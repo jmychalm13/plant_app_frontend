b977c06bacd7dc4d3b3d56c02aec9df90196fb2d
“Jessica <jmychalm13@gmail.com>
2023-12-21 19:24:22 -0600

    added new fertilizer schedule functionality

--- src/FertilizerScheduleNew.jsx
+++ src/FertilizerScheduleNew.jsx
@@ -1,7 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export function FertilizerScheduleNew() {
+  const [plants, setPlants] = useState([]);
+
+  const getPlantValues = () => {
+    axios.get("http://localhost:3000/dropdowns/plant.json").then((response) => {
+      setPlants(response.data);
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    axios
+      .post("http://localhost:3000/fertilizer_schedules.json", params)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error.response.data.errors);
+      });
+  };
+
+  useEffect(getPlantValues, []);
+
   return (
     <div>
-      <h1>Here I Am!!!!</h1>
+      <h1>New Fertilizer Schedule</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="user_plant_id">Plant:</label>
+        <select name="user_plant_id" id="user_plant_id">
+          {plants.map((plant) => (
+            <option key={plant.id} value={plant.id}>
+              {plant.name}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="schedule">Schedule:</label>
+        <input type="text" name="schedule" />
+        <button className="btn btn-primary" type="submit">
+          Add Schedule
+        </button>
+      </form>
     </div>
   );
 }

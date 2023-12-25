8a8ddfe96f00035fc2675ea79d03b881aa62d076
“Jessica <jmychalm13@gmail.com>
2023-12-25 07:09:50 -0600

    added dropdown for zone in modal - need to make it function and add axios patch request

--- src/UserPlantsShow.jsx
+++ src/UserPlantsShow.jsx
@@ -1,7 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export function UserPlantsShow(props) {
+  const [zones, setZones] = useState([]);
+
   const handleSubmit = (event) => {
     event.preventDefault();
   };
+
+  const getZoneValues = () => {
+    axios.get("http://localhost:3000/dropdowns/zone.json").then((response) => {
+      setZones(response.data);
+    });
+  };
+
+  // Todo: make this function work
+  const updateZone = (event) => {
+    console.log(event.target);
+  };
+
+  useEffect(getZoneValues, []);
 
   return (
     <div className="container">
@@ -14,7 +32,24 @@ export function UserPlantsShow(props) {
         </div>
         <div className="form-group">
           Zone:
-          {/* To Do: Figure out dropdowns in Modal */}
+          <select
+            name="zone_name"
+            value={props.plant.zone_name}
+            id="zone_name"
+            onChange={(event) => updateZone(event.target)}
+          >
+            {zones.map((zone) => (
+              <option key={zone.id} value={zone.id}>
+                {zone.location_name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="form-group">
+          Watering Schedules:
+          {props.plant.watering_schedules.map((schedule, index) => (
+            <p key={index}>{schedule.schedule}</p>
+          ))}
         </div>
       </form>
     </div>

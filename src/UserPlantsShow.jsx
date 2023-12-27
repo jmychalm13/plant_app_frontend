fbaf3fdd94e117a44bddf4e73b34c269ff1febbd
“Jessica <jmychalm13@gmail.com>
2023-12-26 18:08:35 -0600

    not sure, something in show component

--- src/UserPlantsShow.jsx
+++ src/UserPlantsShow.jsx
@@ -2,11 +2,26 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export function UserPlantsShow(props) {
+  const initialWateringSchedules = Array.isArray(props.plant.watering_schedules)
+    ? props.plant.watering_schedules.map((schedule) => schedule.schedule)
+    : [];
+
+  const initialFertilizerSchedules = Array.isArray(props.plant.fertilizer_schedules)
+    ? props.plant.fertilizer_schedules.map((schedule) => schedule.schedule)
+    : [];
+
   const [zones, setZones] = useState([]);
   const [types, setTypes] = useState([]);
+  const [wateringSchedules, setWateringSchedules] = useState(initialWateringSchedules);
+  const [fertilizerSchedules, setFertilizerSchedules] = useState(initialFertilizerSchedules);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    const params = new FormData(event.target);
+    props.onUpdatePlant(props.plant.id, params);
+    props.closeModal();
+    event.target.reset();
+    console.log("params", params);
   };
 
   const getZoneValues = () => {
@@ -19,6 +34,18 @@ export function UserPlantsShow(props) {
     axios.get("http://localhost:3000/dropdowns/type.json").then((response) => {
       setTypes(response.data);
     });
+  };
+
+  const handleWaterScheduleChange = (index, newValue) => {
+    const updatedWateringSchedules = [...wateringSchedules];
+    updatedWateringSchedules[index] = newValue;
+    setWateringSchedules(updatedWateringSchedules);
+  };
+
+  const handleFertilizerScheduleChange = (index, newValue) => {
+    const updatedFertilizerSchedules = [...fertilizerSchedules];
+    updatedFertilizerSchedules[index] = newValue;
+    setFertilizerSchedules(updatedFertilizerSchedules);
   };
 
   useEffect(getZoneValues, []);
@@ -36,7 +63,7 @@ export function UserPlantsShow(props) {
         </div>
         <div className="form-group">
           <strong>Zone:</strong>
-          <select name="zone_name" defaultValue={props.plant.zone_name} id="zone_name">
+          <select name="zone_id" defaultValue={props.plant.zone_name} id="zone_id">
             {zones.map((zone) => (
               <option key={zone.id} value={zone.id}>
                 {zone.location_name}
@@ -56,16 +83,28 @@ export function UserPlantsShow(props) {
         </div>
         <div className="form-group">
           <strong>Watering Schedules:</strong>
-          {props.plant.watering_schedules.map((schedule, index) => (
-            <p key={index}>{schedule.schedule}</p>
+          {wateringSchedules.map((schedule, index) => (
+            <input
+              key={index}
+              type="text"
+              value={schedule}
+              onChange={(e) => handleWaterScheduleChange(index, e.target.value)}
+            ></input>
           ))}
         </div>
         <div className="form-group">
           <strong>Fertilizer Schedules:</strong>
-          {props.plant.fertilizer_schedules.map((schedule, index) => (
-            <p key={index}>{schedule.schedule}</p>
+          {fertilizerSchedules.map((schedule, index) => (
+            <input
+              key={index}
+              type="text"
+              value={schedule}
+              onChange={(e) => handleFertilizerScheduleChange(index, e.target.value)}
+            ></input>
           ))}
         </div>
+        <button type="submit">Update</button>
+        <button>Delete</button>
       </form>
     </div>
   );

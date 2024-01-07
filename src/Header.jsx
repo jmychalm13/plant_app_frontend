5b2d332f2eaf27940215f4dc6ff0b90eabb9f822
“Jessica <jmychalm13@gmail.com>
2024-01-07 13:33:03 -0600

    fixed logout link

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import { LogoutLink } from "./LogoutLink";
 
 export function Header() {
   let authLinks;
-  if (localStorage.jwt === undefined) {
+  if (localStorage["jwt"] === undefined) {
     authLinks = (
       <>
         <ul className="navbar-nav">
@@ -85,28 +85,5 @@ export function Header() {
       </div>
       {authLinks}
     </nav>
-    // <header>
-    //   <nav>
-    //     <Link className="nav-link" to="/">
-    //       Home
-    //     </Link>
-    //     <Link className="nav-link" to="/new">
-    //       New Plant
-    //     </Link>
-    //     <Link className="nav-link" to="/search">
-    //       Search
-    //     </Link>
-    //     <Link className="nav-link" to="/new_type">
-    //       New Type
-    //     </Link>
-    //     <Link className="nav-link" to="/new_zone">
-    //       New Zone
-    //     </Link>
-    //     <Link className="nav-link" to="/new_water">
-    //       New Water
-    //     </Link>
-    //     {authLinks}
-    //   </nav>
-    // </header>
   );
 }
